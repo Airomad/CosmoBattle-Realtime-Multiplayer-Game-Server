0496d2ed020cc386d610afa96feb46e1676faac8
Airomad <airomad7@gmail.com>
2018-01-16 00:52:55 +0200

    Added java_websocket library

--- src/main/kotlin/io/github/airomad/cosmobattle/server/Launcher.kt
+++ src/main/kotlin/io/github/airomad/cosmobattle/server/Launcher.kt
@@ -1,11 +1,14 @@
 package io.github.airomad.cosmobattle.server
 
+import java.net.InetSocketAddress
+
 /**
  * Author: Airomad
  * created: 15.01.18
  */
 fun main(args: Array<String>) {
+    val host = "localhost"
+    val port = 8081
 
-    Server().start()
-
+    Server(InetSocketAddress(host, port)).run()
 }
--- src/main/kotlin/io/github/airomad/cosmobattle/server/Server.kt
+++ src/main/kotlin/io/github/airomad/cosmobattle/server/Server.kt
@@ -1,7 +1,34 @@
 package io.github.airomad.cosmobattle.server
 
-class Server {
-    fun start() {
-        println("Project is ready")
+import org.java_websocket.WebSocket
+import org.java_websocket.handshake.ClientHandshake
+import org.java_websocket.server.WebSocketServer
+import java.lang.Exception
+import java.net.InetSocketAddress
+
+
+class Server(private val inetAddress: InetSocketAddress) : WebSocketServer(inetAddress) {
+
+    override fun onOpen(conn: WebSocket?, handshake: ClientHandshake?) {
+        broadcast("Connected new client: ${handshake?.resourceDescriptor}")
+        println("Connected new client: ${conn?.remoteSocketAddress}")
     }
+
+    override fun onClose(conn: WebSocket?, code: Int, reason: String?, remote: Boolean) {
+        println("Disconnected client: ${conn?.remoteSocketAddress}")
+    }
+
+    override fun onMessage(conn: WebSocket?, message: String?) {
+        broadcast("[${conn?.remoteSocketAddress}] $message")
+        println("[${conn?.remoteSocketAddress}] $message")
+    }
+
+    override fun onStart() {
+        println("Server successfully started on ${this.inetAddress.hostName}:${this.inetAddress.port}.")
+    }
+
+    override fun onError(conn: WebSocket?, ex: Exception?) {
+        println("Error catch client: ${conn?.remoteSocketAddress}")
+    }
+
 }
